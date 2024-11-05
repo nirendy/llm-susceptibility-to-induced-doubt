0fc264a08c38610cf2bedf8d48696e2f18504f55
nirendy <nirendy@gmail.com>
2024-11-05 07:08:50 +0200

    adding doubt with logits

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,30 @@
 import torch
 import string
 
+def format_prompt(
+    question_frasing: str,
+    question: str,
+    first_answer: str,
+    second_answer: str,
+    prefix: str = "",
+):
+    """
+    Formats a prompt with placeholders for the question and answers.
+    Args:
+        question_frasing (str): The template for phrasing the question, containing placeholders for answers and the question itself.
+        question (str): The question to be asked.
+        correct_answer (str): The correct answer to the question.
+        incorrect_answer (str): The incorrect answer to the question.
+        prefix (str, optional): A prefix to add to the prompt. Defaults to "".
+    Returns:
+        str: The formatted prompt.
+    """
+    
+    return prefix + question_frasing.replace(
+        "{first_answer}", first_answer
+    ).replace("{second_answer}", second_answer).replace("{question}", question)
+    
+
 def generate_a_or_b(
     pipeline,
     question_frasing: str,
@@ -9,7 +33,7 @@ def generate_a_or_b(
     incorrect_answer: str,
     sample_answer: bool = False,
     max_length: int = 2,
-    prefix: str = ""
+    prefix: str = "",
 ):
     """
     Generates a prompt and evaluates the model's response to determine if it matches the expected answer.
@@ -31,11 +55,11 @@ def generate_a_or_b(
     # Construct a prompt that encourages a short answer
     if torch.rand(1) < 0.5:
         answer = "a"
-        full_prompt = prefix + question_frasing.replace("{first_answer}", correct_answer).replace("{second_answer}", incorrect_answer).replace("{question}", question)
+        full_prompt = format_prompt(question_frasing, question, correct_answer, incorrect_answer, prefix)
     else:
         answer = "b"
-        full_prompt = prefix + question_frasing.replace("{first_answer}", incorrect_answer).replace("{second_answer}", correct_answer).replace("{question}", question)
-    
+        full_prompt = format_prompt(question_frasing, question, incorrect_answer, correct_answer, prefix)
+
     full_prompt = prefix + full_prompt
     # Generate the output
     output = pipeline(full_prompt, max_new_tokens=max_length, num_return_sequences=1, 
